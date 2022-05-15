eb07898f0fcb074a031af3ed828fcd7214da2119
aaboundi <aadja@gmail.com>
2022-05-15 16:38:24 -0700

    error

--- error_handle.c
+++ error_handle.c
@@ -6,29 +6,29 @@
  */
 void errorHandler(config *build)
 {
-  register int len;
-  static char error[BUFSIZE];
-  char *ptr, *alpha;
+	register int len;
+	static char error[BUFSIZE];
+	char *ptr, *alpha;
 
-  alpha = ownItoa(build->lineCounter);
-  _strcat(error, build->shellName);
-  _strcat(error, ": ");
-  _strcat(error, alpha);
-  _strcat(error, ": ");
-  _strcat(error, build->args[0]);
-  _strcat(error, getErrorMessage());
-  if (build->args[1])
-  {
-    if (errno != EBADCD)
-      _strcat(error, ": ");
-    _strcat(error, build->args[1]);
-  }
-  _strcat(error, "\n");
-  ptr = _strchr(error, '\n');
-  len = ptr - error;
-  write(STDERR_FILENO, error, len + 1);
-  free(alpha);
-  insertNullByte(error, 0);
+	alpha = itoa(build->lineCounter);
+	_strcat(error, build->shellName);
+	_strcat(error, ": ");
+	_strcat(error, alpha);
+	_strcat(error, ": ");
+	_strcat(error, build->args[0]);
+	_strcat(error, getErrorMessage());
+	if (build->args[1])
+	{
+		if (errno != EBADCD)
+			_strcat(error, ": ");
+		_strcat(error, build->args[1]);
+	}
+	_strcat(error, "\n");
+	ptr = _strchr(error, '\n');
+	len = ptr - error;
+	write(STDERR_FILENO, error, len + 1);
+	free(alpha);
+	insertNullByte(error, 0);
 }
 
 /**
@@ -37,33 +37,33 @@ void errorHandler(config *build)
  */
 char *getErrorMessage(void)
 {
-  char *str;
+	char *str;
 
-  switch (errno)
-  {
-  case EBADCD:
-    str = ": can't cd to ";
-    break;
-  case ENOENT:
-    str = ": not found";
-    break;
-  case ENOSTRING:
-    str = ": bad variable name";
-    break;
-  case EILLEGAL:
-    str = ": Illegal number";
-    break;
-  case EWSIZE:
-    str = ": invalid number of arguments";
-    break;
-  case ENOBUILTIN:
-    str = ": type help for a list of built-ins";
-    break;
-  case EACCES:
-    str = ": Permission denied";
-    break;
-  default:
-    str = ": no error number assigned";
-  }
-  return (str);
+	switch (errno)
+	{
+		case EBADCD:
+			str = ": can't cd to ";
+			break;
+		case ENOENT:
+			str = ": not found";
+			break;
+		case ENOSTRING:
+			str = ": bad variable name";
+			break;
+		case EILLEGAL:
+			str = ": Illegal number";
+			break;
+		case EWSIZE:
+			str = ": invalid number of arguments";
+			break;
+		case ENOBUILTIN:
+			str = ": type help for a list of built-ins";
+			break;
+		case EACCES:
+			str = ": Permission denied";
+			break;
+		default:
+			str = ": no error number assigned";
+	}
+	return (str);
 }
