3da71a09f0b34fac00a194fc21e9a20bdb6d2203
aaboundi <aadja@gmail.com>
2022-05-15 15:32:36 -0700

    main

--- main.c
+++ main.c
@@ -8,14 +8,14 @@
  */
 int main(int ac, char **av)
 {
-  config build;
+	config build;
 
-  (void)ac;
-  signal(SIGINT, sigintHandler);
-  configInit(&build);
-  build.shellName = av[0];
-  shell(&build);
-  return (0);
+	(void)ac;
+	signal(SIGINT, sigintHandler);
+	configInit(&build);
+	build.shellName = av[0];
+	shell(&build);
+	return (0);
 }
 
 /**
@@ -25,14 +25,14 @@ int main(int ac, char **av)
  */
 config *configInit(config *build)
 {
-  build->env = generateLinkedList(environ);
-  build->envList = NULL;
-  build->args = NULL;
-  build->buffer = NULL;
-  build->path = _getenv("PATH", environ);
-  build->fullPath = NULL;
-  build->lineCounter = 0;
-  build->shellName = NULL;
-  build->errorStatus = 0;
-  return (build);
+	build->env = generateLinkedList(environ);
+	build->envList = NULL;
+	build->args = NULL;
+	build->buffer = NULL;
+	build->path = _getenv("PATH", environ);
+	build->fullPath = NULL;
+	build->lineCounter = 0;
+	build->shellName = NULL;
+	build->errorStatus = 0;
+	return (build);
 }
