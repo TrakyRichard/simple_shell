c4a570b2b38d8dd464583c8cf81a84216ad077ab
Timothée Richard <44176825+TrakyRichard@users.noreply.github.com>
2022-05-17 23:23:40 +0000

    Update the file of string2

--- string2.c
+++ string2.c
@@ -9,22 +9,21 @@
 
 char *_strtok(char *str, char *delim)
 {
-  static char *lastptr;
-  char ch;
+	static char *lastptr;
+	char ch;
 
-  if (str == NULL)
-    str = lastptr;
-  do
-  {
-    ch = *str++;
-    if (!ch)
-      return (NULL);
-  } while (_strchr(delim, ch));
-  str--;
-  lastptr = str + _strcspn(str, delim);
-  if (*lastptr)
-    *lastptr++ = 0;
-  return (str);
+	if (str == NULL)
+		str = lastptr;
+	do {
+		ch = *str++;
+		if (!ch)
+			return (NULL);
+	} while (_strchr(delim, ch));
+	str--;
+	lastptr = str + _strcspn(str, delim);
+	if (*lastptr)
+		*lastptr++ = 0;
+	return (str);
 }
 
 /**
@@ -36,14 +35,14 @@ char *_strtok(char *str, char *delim)
  */
 int _strcspn(char *string, char *chars)
 {
-  char c;
-  char *p, *s;
+	char c;
+	char *p, *s;
 
-  for (s = string, c = *s; c; s++, c = *s)
-    for (p = chars; *p; p++)
-      if (c == *p)
-        return (s - string);
-  return (s - string);
+	for (s = string, c = *s; c; s++, c = *s)
+		for (p = chars; *p; p++)
+			if (c == *p)
+				return (s - string);
+	return (s - string);
 }
 
 /**
@@ -54,14 +53,14 @@ int _strcspn(char *string, char *chars)
  */
 char *_strchr(char *s, char c)
 {
-  char x;
+	char x;
 
-  while (true)
-  {
-    x = *s++;
-    if (x == c)
-      return (s - 1);
-    if (!x)
-      return (NULL);
-  }
+	while (true)
+	{
+		x = *s++;
+		if (x == c)
+			return (s - 1);
+		if (!x)
+			return (NULL);
+	}
 }
